e0f3aa82133b9d8f8a68c7328b8f6ef5f8c1564b
Yuri Kuzmichev <kuzmichev@it-tian.ru>
2024-05-03 18:03:19 +0500

    add components

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,39 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import CalendarPage from "./CalendarPage.jsx";
+import React, { useState } from "react";
+import { createRoot } from "react-dom/client";
+import Calendar from "./Calendar";
+import jsonData from "./calendar.json";
+import WeatherContainer from "./Weather";
 import "./index.css";
+import YearlyProgressCircle from "./YearlyProgressCircle";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <CalendarPage />
-  </React.StrictMode>
-);
+const App = () => {
+  const [showWeatherPopup, setShowWeatherPopup] = useState(false);
+
+  const toggleWeatherPopup = () => {
+    setShowWeatherPopup(!showWeatherPopup);
+  };
+
+  const now = new Date();
+  const startOfYear = new Date(now.getFullYear(), 0, 0);
+  const diff = now - startOfYear;
+  const oneDay = 1000 * 60 * 60 * 24;
+  const daysPassed = Math.floor(diff / oneDay);
+
+  return (
+    <React.StrictMode>
+      <button onClick={toggleWeatherPopup} className="showWeather">
+        Показать погоду
+      </button>
+      <YearlyProgressCircle daysPassed={daysPassed} />
+      <Calendar yearData={jsonData} />
+      {showWeatherPopup && (
+        <div className="weather-popup">
+          <WeatherContainer />
+          <button onClick={toggleWeatherPopup}>Закрыть</button>
+        </div>
+      )}
+    </React.StrictMode>
+  );
+};
+
+createRoot(document.getElementById("root")).render(<App />);
