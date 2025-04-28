f7dc4f4c0bc5eacd83852b62fd76734adc8763c6
Yuri Kuzmichev <kuzmichev@it-tian.ru>
2025-04-28 15:10:53 +0500

    refactoring code

--- generateCalendar.js
+++ generateCalendar.js
@@ -1,15 +1,8 @@
-// Скрипт генерирующий календарь в формате json вот такого вида:
-/* {
-  "year": 2024,
-  "Январь": [
-    [0, 1, 2, 3, 4, 5, 6],
-    [7, 8, 9, 10, 11, 12, 13],
-    [14, 15, 16, 17, 18, 19, 20],
-    [21, 22, 23, 24, 25, 26, 27],
-    [28, 29, 30, 31, 0, 0, 0]
-  ],
-  ---
- */
+import fs from "fs";
+function getMondayBasedWeekday(date) {
+  return (date.getDay() + 6) % 7;
+}
+
 function createCalendar(year) {
   const months = [
     "Январь",
@@ -27,50 +20,43 @@ function createCalendar(year) {
   ];
   const daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
-  // Check for leap year and adjust February days
+  // високосный год
   if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
     daysInMonth[1] = 29;
   }
 
-  let calendar = {
-    year: year,
-  };
+  const calendar = { year };
 
-  months.forEach((month, index) => {
-    let monthArray = [];
-    let firstDay = new Date(year, index, 1).getDay(); // Day of the week the month starts on
-    let days = daysInMonth[index];
+  months.forEach((monthName, mi) => {
+    const dim = daysInMonth[mi];
+    const monthArray = [];
     let week = new Array(7).fill(0);
+    let wday = getMondayBasedWeekday(new Date(year, mi, 1));
 
-    // Fill the first week with leading zeros until the first day
-    for (let i = 0; i < firstDay; i++) {
-      week[i] = 0;
-    }
-
-    // Fill the days of the month
-    for (let day = 1; day <= days; day++) {
-      week[firstDay] = day;
-      firstDay++;
-      if (firstDay === 7) {
+    for (let d = 1; d <= dim; d++) {
+      week[wday] = d;
+      wday++;
+      if (wday === 7) {
         monthArray.push(week);
         week = new Array(7).fill(0);
-        firstDay = 0;
+        wday = 0;
       }
     }
-
-    // Fill the last week with trailing zeros if necessary
-    if (week.some((day) => day !== 0)) {
+    // остаток последней недели
+    if (week.some((v) => v !== 0)) {
       monthArray.push(week);
     }
 
-    calendar[month] = monthArray;
+    calendar[monthName] = monthArray;
   });
 
   return calendar;
 }
 
-const currentYear = new Date().getFullYear();
-const calendar2024 = createCalendar(currentYear);
+const calCurrent = createCalendar(new Date().getFullYear());
 
-// А здесь создать файл формата json с календарём текущего года и поместить туда все эти данные.
-console.log(JSON.stringify(calendar2024, null, 2));
+fs.writeFileSync(
+  "src/calendar.json",
+  JSON.stringify(calCurrent, null, 2),
+  "utf-8"
+);
